5703d78fe2d8919fe28c0416307e5c6c823a36bb
mirmik <mirmikns@yandex.ru>
2019-04-08 01:15:16 +0300

    'a'

--- impulse_controller/main.cpp
+++ impulse_controller/main.cpp
@@ -7,78 +7,62 @@
 
 #include <addons/ramps/ramps.h>
 
-#include <drivers/timer/avr_timer.h>
-
 #include <math.h>
+#include <nos/print.h>
 
-genos::uartring serial0;
-char rxbuf[16];
-char txbuf[128];
+#include <ralgo/planning/traj.h>
 
-int i = 0;
+//genos::uartring serial0;
+//char rxbuf[16];
+//char txbuf[128];
 
-ramps::ramps_driver<genos::avr::timer16> rampsdrv(&periph::timer1);
+//int i = 0;
 
-void handler(void*) 
-{
-	i++;
+ramps::ramps_driver rampsdrv(&periph::timer1);
+
+//void handler(void*) 
+//{
+//	i++;
 	//if (i == 10) {
-		board::sysled.toggle();
-		i = 0;
+//		board::sysled.toggle();
+//		i = 0;
 	//}
-}
+//}
 
 int main() 
 {
 	board_init();
 	board::usart0.setup(115200);
 	board::usart0.enable();
-	serial0.init(&board::usart0, rxbuf, 16, txbuf, 128);
+	//serial0.init(&board::usart0, rxbuf, 16, txbuf, 128);
 
-	PINOUT[11].mode(GPIO_MODE_OUTPUT);
-
-	rampsdrv.init(16000);
-
-	//periph::timer1.set_overflow_irq_handler(handler, &periph::timer1);
-	//periph::timer1.irq_ovf_enable(true);
-	//periph::timer1.set_wgm(0b0111);
-
-	//periph::timer1.set_compare_a(255);
-	//periph::timer1.set_compare_a(0x3ff / 2);
-	//periph::timer1.set_output_a_mode(0b10);
-
-	//periph::timer1.enable(256);
-	//periph::timer1.debug_print_regs();
-
-	//periph::timer1.irq_overflow_enable(true);
-
-	periph::timer1.debug_print_regs();
-	//pinMode(24, 1);
-	//digitalWrite(24, 1);
-//	schedee_manager_init();
-	irqs_enable();
-
-	periph::timer1.debug_print_regs();
-	periph::timer1.debug_print_regs();
-
-	//serial0.println("HelloWorld");
+	rampsdrv.init(4000, 8);
 
 	ramps::x_driver.enabled=true;
 	ramps::y_driver.enabled=true;
 	ramps::z_driver.enabled=true;
+	
+	irqs_enable();
 
+	auto traj0 = ralgo::accdcc_by_time_trajectory<>(0, 50000, 3000, 10000, 3000); 
+	auto traj1 = ralgo::accdcc_by_time_trajectory<>(0, 25000, 3000, 10000, 3000); 
+	auto traj2 = ralgo::accdcc_by_time_trajectory<>(0, 12500, 3000, 10000, 3000); 
 
-	ramps::x_driver.step_width = 10000;
-	ramps::y_driver.step_width = 20000;
-	ramps::z_driver.step_width = 40000;
+	delay(1000);
 
+	ralgo::phase<> phs0, phs1, phs2;
+	auto stime = millis();
 	while(1) 
 	{
-	//	auto m = millis() % 1000;
-	//	float a = (float)m / 1000 * M_PI * 2;
-	//	periph::timer1.set_compare_a(0x3ff / 2 + sin(a)*0x100);
-		delay(1000);
-		//serial0.println("HelloWorld");
+		auto tm = millis() - stime;
+		traj0.inloctime_placed(tm, &phs0);
+		traj1.inloctime_placed(tm, &phs1);
+		traj2.inloctime_placed(tm, &phs2);
+		ramps::x_driver.write(phs0.d0, phs0.d1);
+		ramps::y_driver.write(phs1.d0, phs1.d1);
+		ramps::z_driver.write(phs2.d0, phs2.d1);
+
+		//ramps::z_driver.print_state();
 	}
 
 
