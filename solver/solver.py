ac6104f2b3603d894d9a36994ed007202352f0f4
mirmik <mirmikns@yandex.ru>
2022-03-18 18:01:40 +0300

    'a'

--- solver/solver.py
+++ solver/solver.py
@@ -7,6 +7,111 @@ import sys
 
 numpy.set_printoptions(threshold=sys.maxsize, linewidth=200, precision=2)
 
+class SimplexFEM2:
+	def __init__(self, vertices):
+		self.density = 1000
+		self.Poisson = 0.25 # 0.25 for steel
+		self.elastic_modulus = 1
+		self.vertices = vertices
+		self.reverse_if_need()
+		self.a, self.b, self.c = self.formal_coefficients()
+		self.S = self.area()
+		self.solver_element_indexes = {}
+
+	def area(self):
+		x, y = self.xy_from_vertices()
+		return numpy.linalg.det([
+			[1,x[0], y[0]],
+			[1,x[1], y[1]],
+			[1,x[2], y[2]],
+		])
+		
+	def reverse_if_need(self):
+		area = self.area()
+		if (area < 0):
+			self.vertices = [ self.vertices[0], self.vertices[2], self.vertices[1] ]
+
+	def xy_from_vertices(self):
+		x = [ v[0] for v in self.vertices ]
+		y = [ v[1] for v in self.vertices ]
+		return x, y
+
+	def cyclic_shift(self, a):
+		return a[1:3] + a[0:1]  
+
+	def vertex_deformation_matrix(self, i):
+		return (
+				numpy.array([
+					[self.b[i],         0],
+					[        0, self.c[i]],
+					[self.c[i], self.b[i]],
+				])
+			)
+
+	def deformation_matrix(self):
+		B0 = self.vertex_deformation_matrix(0)
+		B1 = self.vertex_deformation_matrix(1)
+		B2 = self.vertex_deformation_matrix(2)
+		return numpy.concatenate((B0,B1,B2), axis=1)
+
+	def elastic_matrix(self):
+		E = self.elastic_modulus
+		v = self.Poisson
+		return E*(1-v)/(1+v)/(1-2*v) * numpy.array([
+			[        1, v/(1-v),               0],
+			[  v/(1-v),       1,               0],
+			[        0,       0, (1-2*v)/2/(1-v)],
+		])
+
+
+	def formal_coefficients(self):
+		x, y = self.xy_from_vertices()
+
+		a = [0] * 4
+		b = [0] * 4
+		c = [0] * 4
+		
+		for i in range(4):
+			a_ = numpy.linalg.det([
+				[x[1],y[1]],
+				[x[2],y[2]]
+			])
+
+			b_ = -numpy.linalg.det([
+				[1,y[1]],
+				[1,y[2]]
+			])
+
+			c_ = -numpy.linalg.det([
+				[x[1],1],
+				[x[2],1],
+			])			
+
+			x = self.cyclic_shift(x)
+			y = self.cyclic_shift(y)
+
+			a[i] = a_ * (-1)**i
+			b[i] = b_ * (-1)**i
+			c[i] = c_ * (-1)**i
+
+		return a, b, c
+
+	def stiffness_matrix(self):
+		B = self.deformation_matrix()
+		Bt = B.transpose()
+		D = self.elastic_matrix()
+		return numpy.matmul(Bt, numpy.matmul(D,B))
+
+
+	def stiffness_matrix_as_blocks(self):
+		stiffness = self.stiffness_matrix()
+		blocks = [
+			[stiffness[ 0:3,0:3], stiffness[ 0:3,3:6], stiffness[ 0:3,6:9]],
+			[stiffness[ 3:6,0:3], stiffness[ 3:6,3:6], stiffness[ 3:6,6:9]],
+			[stiffness[ 6:9,0:3], stiffness[ 6:9,3:6], stiffness[ 6:9,6:9]],
+		]
+		return blocks
+
 class SimplexFEM:
 	def __init__(self, vertices):
 		self.density = 1000
@@ -58,9 +163,6 @@ class SimplexFEM:
 					[self.c[i], self.b[i],         0],
 				])
 			)
-
-	def deformation_matrix(self):
-		return self._deformation_matrix
 
 	def deformation_matrix(self):
 		B0 = self.vertex_deformation_matrix(0)
@@ -174,7 +276,11 @@ class FiniteElementSolver:
 		self.index_counter = 0
 
 	def hash_of_vertex(self, v):
-		return hash(v[0]) + hash(v[1])*52435435 + hash(v[2])*241234 
+		if len(v) == 3:
+			return hash(v[0]) + hash(v[1])*52435435 + hash(v[2])*241234 
+		elif len(v) == 2:
+			return hash(v[0]) + hash(v[1])*52435435 +341234
+			
 
 	def get_index_for_vertex(self, vertex):
 		vhash = self.hash_of_vertex(vertex)
@@ -212,6 +318,12 @@ class FiniteElementSolver:
 		vec[index*3:(index+1)*3] = arr
 		return vec
 
+def fems_of_square():
+	return [
+		SimplexFEM2([[0,0],[1,1],[1,0]]), # red
+		SimplexFEM2([[0,0],[1,1],[0,1]]), # blue
+	]
+
 def fems_of_cube():
 	return [
 		SimplexFEM([[0,0,0],[1,1,1],[1,0,1],[0,0,1]]), # red
@@ -228,7 +340,19 @@ def fems_of_cube():
 #		SimplexFEM([[0,0,0],[0,0,1],[-1,0,0],[0,-1,0]]),
 #		SimplexFEM([[0,0,0],[0,0,1],[1,0,0],[0,-1,0]]),
 	]
-		
+
+
+if __name__ == "__main__":
+	fems = fems_of_square()
+
+	solver = FiniteElementSolver()
+	for f in fems:
+		solver.add(f)
+
+	stiffness = solver.stiffness_matrix()
+	print(stiffness)
+
+exit(0)		
 if __name__ == "__main__":
 	fems = fems_of_cube()
 	
