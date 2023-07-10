4ecdafba61bc14c6d1eae57c6c0a395ea9d4b455
mirmik <mirmikns@yandex.ru>
2023-07-10 08:54:45 +0300

    'a'

--- game.py
+++ game.py
@@ -4,7 +4,7 @@ import math
 import matplotlib.pyplot as plt
 import random
 import numpy
-
+import sympy
 
 class Gamer:
     def __init__(self, start_strategy, speed, error, cond_equal):
@@ -17,20 +17,8 @@ class Gamer:
         r = random.random()
         return (self.strategy-self.error) > r
 
-    def predict(self):
-        r = random.random()
-        return self.strategy  > r
-
-    def knowlage(self, B, N):
-        prediction_success = 0
-        for i in range(N):
-            prediction = self.predict()
-            move = B.move()
-            if move == prediction and self.cond_equal:
-                prediction_success += 1
-            if move != prediction and not self.cond_equal:
-                prediction_success += 1
-        return prediction_success / N 
+    def moves(self):
+        return self.strategy-self.error
 
     def update_model(self, win, true_false):
         target = true_false
@@ -39,24 +27,30 @@ class Gamer:
 
         if target:
             self.strategy += self.speed
-            #self.strategy += (1-self.strategy) * 0.001
+            #self.strategy += (1-self.strategy) * self.speed
         else:
             self.strategy -= self.speed
-            #self.strategy -= (self.strategy) * 0.001
+            #self.strategy -= (self.strategy) * self.speed
 
-        self.strategy = min(1, self.strategy)
-        self.strategy = max(0, self.strategy)
+        if self.strategy-self.error > 1:
+            self.strategy = 1 + self.error
+        if self.strategy-self.error < 0:
+            self.strategy = 0 + self.error
        
 
-A = Gamer(0.5, speed=0.0001, error = 0.1, cond_equal = True)
-B = Gamer(0.5, speed=0.000001, error = 0.001, cond_equal = False)
+A = Gamer(0.5, speed=0.00001, error = 0.1, cond_equal = True)
+B = Gamer(0.5, speed=0.00001, error = 0.1, cond_equal = False)
 
 
 #A_knowlage = []
 #B_knowlage = []
 A_strategy = []
 B_strategy = []
+A_moves = []
+B_moves = []
 winners = []
+winfunc = []
+winfunc2 = []
 for i in range(1000000):
     #A_kn = A.knowlage(B,1000)
     #B_kn = B.knowlage(A,1000)
@@ -67,12 +61,19 @@ for i in range(1000000):
 
     A_move = A.move()
     B_move = B.move()
+    A_moves.append(A.moves())
+    B_moves.append(B.moves())
+
+    #print(A_move, B_move, A.strategy, B.strategy)
 
     A_win = A_move == B_move
     winners.append(A_win)
 
     A.update_model(A_win, A_move)
     B.update_model(not A_win, B_move)
+
+    winfunc.append(1+2*A.moves()*B.moves() - A.moves() - B.moves())
+    winfunc2.append(-2*A.moves()*B.moves() + A.moves() + B.moves())
     
 acc = 0
 for i in range(len(winners)):
@@ -81,6 +82,25 @@ for i in range(len(winners)):
 
 print(acc/len(winners))
 
+A_win_count = 0
+B_win_count = 0
+for i in range(len(winners)):
+    if winners[i]:
+        A_win_count += 1
+    else:
+        B_win_count += 1
+
+print(A_win_count)
+print(B_win_count)
+
+sympy.var("x a b y p")
+
+E = sympy.integrate(-2*(a*sympy.cos(p)+x)*(b*sympy.sin(p)+y)+a*sympy.cos(p)+x+b*sympy.sin(p)+y, (p, 0, 2*sympy.pi))
+sympy.pprint(E)
+
 plt.plot(A_strategy, B_strategy, numpy.linspace(0,1,100), numpy.linspace(0,1,100), numpy.linspace(0,1,100), [0.5]*100, [0.5]*100, numpy.linspace(0,1,100))
-#plt.plot(A_model, B_model)
+plt.plot(A_moves, B_moves, numpy.linspace(0,1,100), numpy.linspace(0,1,100), numpy.linspace(0,1,100), [0.5]*100, [0.5]*100, numpy.linspace(0,1,100))
+#plt.plot(range(1000000), A_strategy, range(1000000), B_strategy)
+#plt.plot(range(1000000), winfunc, range(1000000), winfunc2)
+#plt.scatter(range(1000000), winners)
 plt.show()
