3abe3cd69adeeec472c149d9c1d1c5a5fb6ee458
mirmik <mirmikns@yandex.ru>
2023-07-11 18:59:49 +0300

    'a'

---
+++ comb.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+import math
+
+ARR = [4, 3, 1, 4, 1, 2]
+#CUR = [0, 0, 0, 0, 0, 0]
+#ARR = [4, 3, 2, 2]
+CUR = [0] * len(ARR)
+
+
+def rec(level, current, target):
+    rets = 0
+    #print(level, current)
+
+    if current == target:
+        return 1
+
+    for i in range(len(ARR)):
+        cur = list(current)
+        is_last = cur[i] == 0
+        cur[i] += 1
+        if cur[i] <= target[i]:
+            rets += rec(level+1, cur, target)
+        if is_last:
+            break
+    return rets
+
+
+print(rec(0, CUR, ARR))
+A = math.comb(2+2-1, 2-1)
+B = math.comb(2+2+3-1, 3-1) * A
+C = math.comb(2+2+3+4-1, 4-1) * B
+print(C)
