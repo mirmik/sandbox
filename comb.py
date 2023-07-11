89776e7b7fc0a530a36e75b0e8fe8e6aa71ba573
mirmik <mirmikns@yandex.ru>
2023-07-11 19:17:06 +0300

    'a'

--- comb.py
+++ comb.py
@@ -2,15 +2,15 @@
 
 import math
 
-ARR = [4, 3, 1, 4, 1, 2]
-#CUR = [0, 0, 0, 0, 0, 0]
-#ARR = [4, 3, 2, 2]
+# ARR = [4, 3, 1, 4, 1, 2]
+# CUR = [0, 0, 0, 0, 0, 0]
+ARR = [4, 3, 2, 2]
 CUR = [0] * len(ARR)
 
 
 def rec(level, current, target):
     rets = 0
-    #print(level, current)
+    # print(level, current)
 
     if current == target:
         return 1
@@ -30,4 +30,10 @@ print(rec(0, CUR, ARR))
 A = math.comb(2+2-1, 2-1)
 B = math.comb(2+2+3-1, 3-1) * A
 C = math.comb(2+2+3+4-1, 4-1) * B
+print(A)
+print(B)
 print(C)
+
+
+print(math.factorial(2+2+3-1) * math.factorial(2+2-1) / math.factorial(2) /
+      math.factorial(4) / math.factorial(2))
